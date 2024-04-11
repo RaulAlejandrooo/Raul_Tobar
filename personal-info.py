3012f6d6ee1bc2f1b68207e88dca30dbe59024be
NetAcad DEVASC <changeme@example.com>
2024-04-11 01:25:07 +0000

    Raul_Tobar_172042546 Evaluación N°1 DRY7122

---
+++ personal-info.py
@@ -0,0 +1,7 @@
+Nombre = input("cual es tu nombre ")
+apellido = input("cual es tu apellido ")
+Rut = input("cual es tu rut ")
+Duoc_mail = input("cual es tu correo ")
+Duoc_sede = input("cual es tu sede ")
+
+print( Nombre   , apellido   , Rut   , Duoc_mail  , Duoc_sede   )
